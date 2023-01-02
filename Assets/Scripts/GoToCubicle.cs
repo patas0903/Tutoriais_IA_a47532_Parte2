442300ce69706416471c8bbaeb29c2bc0885b031
Patrícia Guerreiro <a47532@alunos.ipb.pt>
2023-01-02 18:49:57 +0000

    Tutorial 4 - Moving the Nurse

--- Assets/Scripts/GoToCubicle.cs
+++ Assets/Scripts/GoToCubicle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetTreated : GAction
+public class GoToCubicle : GAction
 {
     public override bool PrePerform()
     {
@@ -14,8 +14,11 @@ public class GetTreated : GAction
 
     public override bool PostPerform()
     {
-        GWorld.Instance.GetWorld().ModifyState("isTreated", 1);
+        GWorld.Instance.GetWorld().ModifyState("TreatingPatient", 1);
+        GWorld.Instance.AddCubicle(target);
+        
         inventory.RemoveItem(target);
+        GWorld.Instance.GetWorld().ModifyState("FreeCubicle", 1);
         return true;
     }
 
