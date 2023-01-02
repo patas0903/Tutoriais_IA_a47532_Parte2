51263ab6b60fe21bcb1c6faf3dd9cd358abd0269
Patrícia Guerreiro <a47532@alunos.ipb.pt>
2023-01-02 19:33:19 +0000

    Tutorial 4 - Taking a Break

--- Assets/Scripts/Nurse.cs
+++ Assets/Scripts/Nurse.cs
@@ -10,6 +10,17 @@ public class Nurse : GAgent
         base.Start();
         SubGoal s1 = new SubGoal("treatPatient", 1, false);
         goals.Add(s1, 3);
+
+        SubGoal s2 = new SubGoal("rested", 1, false);
+        goals.Add(s2, 3);
+
+        Invoke("GetTired", Random.Range(10, 20));
+    }
+
+    void GetTired()
+    {
+        beliefs.ModifyState("exhausted", 0);
+        Invoke("GetTired", Random.Range(10, 20));
     }
 
     
