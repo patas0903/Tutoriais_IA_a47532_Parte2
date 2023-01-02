be04f04c7765ee00f48dc4097342f6680ffec34a
Patrícia Guerreiro <a47532@alunos.ipb.pt>
2023-01-02 01:51:13 +0000

    Tutorial 4 - Executing a Simple Plan

--- Assets/Scripts/GAgent.cs
+++ Assets/Scripts/GAgent.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class SubGoal {
 
@@ -25,11 +27,11 @@ public class GAgent : MonoBehaviour {
 
     GPlanner planner;
     Queue<GAction> actionQueue;
-    public GAction currentAvtion;
+    public GAction currentAction;
     SubGoal currentGoal;
 
     // Start is called before the first frame update
-    void Start() {
+    public void Start() {
 
         GAction[] acts = this.GetComponents<GAction>();
         foreach (GAction a in acts) {
@@ -38,9 +40,81 @@ public class GAgent : MonoBehaviour {
         }
     }
 
+
+    bool invoked = false;
+    void CompleteAction()
+    {
+        currentAction.running = false;
+        currentAction.PostPerform();
+        invoked = false;
+    }
+
+
+
     // Update is called once per frame
     void LateUpdate() {
 
+
+        if (currentAction != null && currentAction.running)
+        {
+            if (currentAction.agent.hasPath && currentAction.agent.remainingDistance < 1f)
+            {
+                if(!invoked)
+                {
+                    Invoke("CompleteAction", currentAction.duration);
+                    invoked = true;
+                }
+            }
+            return;
+        }
+
+
+
+        if (planner == null || actionQueue == null)
+        {
+            planner = new GPlanner();
+
+            var sortedGoals = from entry in goals orderby entry.Value descending select entry;
+
+            foreach(KeyValuePair<SubGoal, int> sg in sortedGoals)
+            {
+                actionQueue = planner.plan(actions, sg.Key.sGoals, null);
+                if(actionQueue != null)
+                {
+                    currentGoal = sg.Key;
+                    break;
+                }
+            }
+        }
+
+        if (actionQueue != null && actionQueue.Count == 0)
+        {
+            if (currentGoal.remove)
+            {
+                goals.Remove(currentGoal);
+            }
+            planner = null;
+        }
+
+        if(actionQueue != null && actionQueue.Count > 0)
+        {
+            currentAction = actionQueue.Dequeue();
+            if (currentAction.PrePerform()) ;
+            {
+                if (currentAction.target == null && currentAction.targetTag != "")
+                    currentAction.target = GameObject.FindWithTag(currentAction.targetTag);
+            }
+
+            if (currentAction.target != null)
+            {
+                currentAction.running = true;
+                currentAction.agent.SetDestination(currentAction.target.transform.position);
+            }
+        }
+        else
+        {
+            actionQueue = null;
+        }
     }
 }
 
---
+++ Assets/Scripts/Patient.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patient : GAgent
+{
+    
+    void Start()
+    {
+        base.Start();
+        SubGoal s1 = new SubGoal("isWaiting", 1, true);
+        goals.Add(s1, 3);
+    }
+
+    
+}
